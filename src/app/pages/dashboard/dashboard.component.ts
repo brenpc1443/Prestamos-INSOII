df671c41cd112c0699950afa83ad80e7cc32c5e2
Bren pc <brayanp.cabrera@gmail.com>
2024-10-06 18:49:26 -0500

    changues

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  constructor(private router: Router) {}
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+}
